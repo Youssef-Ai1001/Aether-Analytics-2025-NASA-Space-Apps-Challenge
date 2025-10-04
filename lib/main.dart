309d0dadc96658e35b9f9ac2e3a0b2e7856c82fe
abdelsatar777 <mohammedabdelsatar2006@gmail.com>
2025-10-04 10:03:07 +0300

    testing edit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,5 @@ import 'package:flutter/cupertino.dart';
 import 'package:nasa_app/main_app.dart';
 
 void main() {
-  /*WidgetsFlutterBinding.ensureInitialized();
-  FirebaseService.init();*/
   runApp(MainApp());
 }
